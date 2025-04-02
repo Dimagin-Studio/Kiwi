6c0a4f3b7f78a84ca161c4fc6dac61305b9cecfb
ryanfoerster <ryanfoerster@outlook.be>
2025-04-02 16:48:20 +0200

    🔧 fix(logo): corriger le chemin de l'image du logo
    - mettre à jour le chemin de l'image du logo de `../images/lotus.png` à `/images/lotus.png` pour assurer un chargement correct.

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -2,7 +2,7 @@ export function Logo() {
   return (
     <div className="fixed md:top-4 md:left-20 h-[60px] flex items-center z-50 left-4 top-4">
       <img
-        src="../images/lotus.png"
+        src="/images/lotus.png"
         alt="Logo"
         className="h-full object-contain"
       />
