83644653aadd6a2fd3cba025fb2a63d95f0bc934
ryanfoerster <ryanfoerster@outlook.be>
2025-04-03 14:04:18 +0200

    ♻️ refactor(directusService): centraliser l'URL de l'API
    - mise à jour de directusService pour utiliser une constante API_BASE_URL en fonction de l'environnement
    - amélioration de la maintenabilité en évitant les URL codées en dur

--- src/services/directusService.js
+++ src/services/directusService.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.PROD
+  ? "https://kiwi-directus.dimagin.be"
+  : "/api";
+
+// const API_BASE_URL = "https://kiwi-directus.dimagin.be";
+
 export async function getUpcomingEvents() {
   try {
-    const response = await axios.get(`/api/items/events?sort=date`);
+    const response = await axios.get(`${API_BASE_URL}/items/events?sort=date`);
     return response.data.data;
   } catch (error) {
     console.error("Erreur lors de la récupération des événements :", error);
